93269a503886583e2a0e3dca88cb5334d3c2e955
Marcos <MarcoH4502@users.noreply.github.com>
2026-09-20 11:40:12 -0300

    Resolucao-exercicio02

--- exercicio-02/ex02.c
+++ exercicio-02/ex02.c
@@ -1,8 +1,28 @@
 /*
  * Aluno: Marcos
  * Exercício: 02
- * Descrição: Resolução do exercício 02 da Lista de Exercícios 01.
+ * Descrição: Le um numero N e exibe os primeiros N termos de uma sequencia em que cada termo e a soma dos dois anteriores.
  * Data: 19/09/2026
  */
 
+#include <stdio.h>
 
+ int main() {
+
+    int sequencia, proximo, anterior = 0, atual = 1;
+
+    printf("Digite um numero inteiro maior do que zero para a sequencia: "); scanf("%d", &sequencia);
+
+    if (sequencia <= 0) {
+        printf("Numero da sequencia e invalido");
+    } else {
+    for (int i = 0; i <= sequencia-1; i++) {
+        printf("%d ", atual);
+        proximo = anterior + atual;
+        anterior = atual; 
+        atual = proximo;   
+    }
+    }
+
+    return 0;
+ }
